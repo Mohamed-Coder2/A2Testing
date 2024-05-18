6b5a949d3cc6e5e84538673691bfa2d783e4db60
Mohamed <mohamedemad8862@gmail.com>
2024-05-18 13:32:46 +0300

    Added invalid search testing

--- src/main/java/org/example/imdbTesting.java
+++ src/main/java/org/example/imdbTesting.java
@@ -3,55 +3,11 @@ package org.example;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-public class imdbTesting {
-    @Test
-    public void testSearch() throws InterruptedException {
-        // Set the path to GeckoDriver
-        System.setProperty("webdriver.firefox.driver", "E:\\Moo\\Chrome Downloads\\geckodriver-v0.34.0-win-aarch64\\geckodriver.exe");
-
-        // Initialize Firefox driver
-        WebDriver webDriver = new FirefoxDriver();
-
-        // Navigate to IMDb
-        webDriver.navigate().to("https://www.imdb.com");
-
-        // Wait for the search bar to be visible
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-        WebElement searchBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("suggestion-search")));
-
-        // Enter text into the search bar using JavaScript
-        // I'm using this in particular because accessing by class name normally
-        // doesn't work (selenium can't find the bar using keyboard) ?
-        JavascriptExecutor js = (JavascriptExecutor) webDriver;
-        js.executeScript("arguments[0].value='One Piece';", searchBar);
-        Thread.sleep(2000);
-
-        // Click on the search button
-        WebElement searchButton = webDriver.findElement(By.id("suggestion-search-button"));
-        searchButton.click();
-        Thread.sleep(2000);
-
-        // Check if the first result has the name of the title we searched for
-        String actual = webDriver.findElement(By.className("ipc-metadata-list-summary-item__t")).getText();
-        String expected = "One Piece";
-
-        // Assert
-        Assert.assertEquals(expected,actual);
-        Thread.sleep(2000);
-
-        // Close the browser
-        webDriver.quit();
-    }
-
+public class IMDbTesting {
     @Test
     public void testTop250() throws InterruptedException {
         // If you want to test it yourself don't forget to change these hardcoded values
